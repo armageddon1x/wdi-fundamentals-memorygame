2d358397e860c390116b56eeab8f666c28caa470
armageddon <armageddon1x@gmail.com>
2018-02-20 20:46:48 -0500

    created game logic

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!");
+
+//variables
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFOur = "king";
+
+//console log
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
