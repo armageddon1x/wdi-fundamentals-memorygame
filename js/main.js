60f20b2106b49dc4230a9a35d8d1430962b8e5d7
armageddon <armageddon1x@gmail.com>
2018-03-01 16:26:53 -0500

    Added game logic

--- js/main.js
+++ js/main.js
@@ -23,20 +23,47 @@ var cards = [
 	}
 ];
 var cardsInPlay = [];
+var win = 0;
+var lose = 0;
 
 //function definition
+var reset = function(){
+	var resetCards = document.getElementsByTagName('img');
+	for(var i = 0; i < cards.length; i++){
+		resetCards[i].setAttribute('src','images/back.png');
+	}
+	cardsInPlay = [];
+}
+
 var checkForMatch = function(){
+	//variables
+	var message = document.getElementById('game_message');
+
+	var scoreCardWin = document.getElementById('score_won');
+	var scoreCardLose = document.getElementById('score_lost');
+
+	var button = document.getElementsByTagName('button')[0];
+	console.log(button);
+
 	//if loop
 	if (cardsInPlay.length === 2){
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
+			message.textContent = "You found a match!";
+			win++;
+			scoreCardWin.textContent = win;
 		}else{
-			alert("Sorry, try again.");
+			message.textContent = "Sorry, try again.";
+			lose++;
+			scoreCardLose.textContent = lose;
 		}
+
+		button.addEventListener('click', reset);
+		//reset();
 	}
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	//console log
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
@@ -44,9 +71,20 @@ var flipCard = function(cardId){
 
 	cardsInPlay.push(cards[cardId].rank);
 
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	checkForMatch();
 }
 
+var createBoard = function(){
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src',"images/back.png");
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
 //function call
-flipCard(0);
-flipCard(2);
+createBoard();
